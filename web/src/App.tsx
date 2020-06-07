bd4c8cd16f0e5330d07d12669e5499951c1e9854
App React Native <appreactnative@Air-de-App.popfibra>
2020-06-07 12:52:26 -0300

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+// JSX: Html dentro do JavaScript
+
+function App() {
+  return (
+     <Routes />
+  );
+}
+
+export default App;
